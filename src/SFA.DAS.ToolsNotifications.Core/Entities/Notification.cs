ca6d22ec3b04ba6a34e499b94ff22d983d03f32e
Craig Gumbley <craiggumbley@gmail.com>
2020-04-06 12:41:22 +0100

    Add jsonpropery attribute

--- src/SFA.DAS.ToolsNotifications.Core/Entities/Notification.cs
+++ src/SFA.DAS.ToolsNotifications.Core/Entities/Notification.cs
@@ -1,9 +1,16 @@
-﻿namespace SFA.DAS.ToolsNotifications.Core.Entities
+﻿using Newtonsoft.Json;
+
+namespace SFA.DAS.ToolsNotifications.Core.Entities
 {
     public class Notification
     {
+        [JsonProperty("title", Required = Required.Always)]
         public string Title { get; set; }
+
+        [JsonProperty("description", Required = Required.Always)]
         public string Description { get; set; }
+
+        [JsonProperty("enabled", Required = Required.Always)]
         public bool Enabled { get; set; }
     }
 }
