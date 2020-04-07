fb10090273727dd0b9ecc37b6905ff35303936cf
Craig Gumbley <craiggumbley@gmail.com>
2020-04-07 22:17:08 +0100

    adds return services

--- src/SFA.DAS.ToolsNotifications.Client/ServiceCollectionExtension.cs
+++ src/SFA.DAS.ToolsNotifications.Client/ServiceCollectionExtension.cs
@@ -7,10 +7,12 @@ namespace SFA.DAS.ToolsNotifications.Client
 {
     public static class ServiceRegistrationExtension
     {
-        public static void AddNotificationClient(this IServiceCollection services, IOptions<NotificationClientConfiguration> configuration)
+        public static IServiceCollection AddNotificationClient(this IServiceCollection services, IOptions<NotificationClientConfiguration> configuration)
         {
             var notificationRedisRepository = new NotificationRedisRepository(configuration.Value);
             services.AddSingleton<INotificationClient>(new NotificationClient(notificationRedisRepository));
+
+            return services;
         }
     }
 }
