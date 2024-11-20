fb7f180d92f8dde93fe1b318f7fc27730aa8d88b
Divesh Mavji <divesh987@hotmail.co.uk>
2024-11-20 09:49:05 +0000

    Add retry to Test-Api.ps1

--- tests/Test-Api.ps1
+++ tests/Test-Api.ps1
@@ -1,6 +1,22 @@
 cd $PSScriptRoot/..
 docker-compose up --detach
-Start-Sleep -Seconds 5 # do better
+
+# Retry mechanism to wait for the service to be ready
+$maxRetries = 10
+$retry = 0
+while ($retry -lt $maxRetries) {
+    try {
+        Invoke-RestMethod -Method GET -Uri "http://localhost:8000/health"
+        break
+    } catch {
+        Start-Sleep -Seconds 5
+        $retry++
+        if ($retry -eq $maxRetries) {
+            throw "Service did not start in time"
+        }
+    }
+}
+
 $Body = @"
 {
     "title": "test1",
@@ -8,10 +24,10 @@ $Body = @"
     "enabled": true
 }
 "@
-Invoke-RestMethod -Method POST -Uri "http://localhost:8000/" -Headers @{ "Content-Type"="application/json" } -Body $Body
+Invoke-RestMethod -Method POST -Uri "http://localhost:8000/" -Headers @{ "Content-Type" = "application/json" } -Body $Body
 $TestResponse = Invoke-RestMethod -Method GET -Uri "http://localhost:8000/"
 Write-Host $TestResponse
 $Assert = Compare-Object $TestResponse ($Body | ConvertFrom-Json)
-if($null -ne $Assert){
+if ($null -ne $Assert) {
     throw "Test assertion failed"
 }
